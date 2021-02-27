fd997e41fedaba88909b1b75e12de68a1e44cc77
Olga <olga.zinch.mail@gmail.com>
2021-02-27 13:24:11 +0200

    Lesson 4: improvements

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -8,34 +8,49 @@
 
 import Foundation
 
-class Concentration {
+struct Concentration {
     
-    var cards = [Card]()
+    private(set) var cards = [Card]()
     
-    var indexOfOneAndOnlyOneFaceUpCard: Int?
+    private var indexOfOneAndOnlyOneFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
     
     private var flipCount = 0
     
-    func chooseCard(at index: Int) {
+    mutating func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): chosen index not in the cards")
         if let matchIndex = indexOfOneAndOnlyOneFaceUpCard, matchIndex != index {
             // check if cards match
-            if cards[matchIndex].identifier == cards[index].identifier {
+            if cards[matchIndex] == cards[index] {
                 cards[matchIndex].isMatched = true
                 cards[index].isMatched = true
             }
             cards[index].isFaceUp = true
-            indexOfOneAndOnlyOneFaceUpCard = nil
         } else {
             // either no cards  or 2 cards are face up
-            for flipDowmIndex in cards.indices {
-                cards[flipDowmIndex].isFaceUp = false
-            }
-            cards[index].isFaceUp = true
             indexOfOneAndOnlyOneFaceUpCard = index
         }
     }
     
-    func incrementFlipCount() -> Int {
+    mutating func incrementFlipCount() -> Int {
         flipCount += 1
         return flipCount
     }
@@ -45,6 +60,7 @@ class Concentration {
     }
 
     init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "Concentration.init(at: \(numberOfPairsOfCards)): you must have at least one pair of cards")
         for _ in 0..<numberOfPairsOfCards {
             let card = Card()
             cards += [card, card]
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,63 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+    private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
     
-    static let halloweenTheme = Theme(
+    var numberOfPairsOfCards: Int {
+        return (cardButtons.count + 1) / 2
+    }
+    
+    private static let halloweenTheme = Theme(
         emojis: ["🦇","😱","🙀","😈","🎃","👻","🍭","🍬","🍎", "🧛‍♂️", "🧟‍♀️", "🧙‍♂️"],
         backgroundColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1),
         cardColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
     )
     
-    static let facesTheme = Theme(
+    private static let facesTheme = Theme(
         emojis: ["😀","😆","😅","🤣","😇","😉","😍","😋","😝", "😎", "🥺", "🤬"],
         backgroundColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1),
         cardColor: #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
     )
     
-    static let handsTheme = Theme(
+    private static let handsTheme = Theme(
         emojis: ["🙌","👍","🤘","🖖","🤙","✊","👌","👏","👆", "💪", "☝️", "🤝"],
         backgroundColor: #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1),
         cardColor: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
     )
     
-    static let sportsTheme = Theme(
+    private static let sportsTheme = Theme(
         emojis: ["⚽️","🏀","🏈","⚾️","🥎","🎾","🏐","🏉","🥏", "🎱", "🏓", "🏸"],
         backgroundColor: #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1),
         cardColor: #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
     )
     
-    static let animalsTheme = Theme(
+    private static let animalsTheme = Theme(
         emojis: ["🐶","🐱","🐭","🐹","🐰","🦊","🐻","🐼","🐨", "🐯", "🦁", "🐷"],
         backgroundColor: #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1),
         cardColor: #colorLiteral(red: 0.9995340705, green: 0.988355577, blue: 0.4726552367, alpha: 1)
     )
     
-    static let natureTheme = Theme(
+    private static let natureTheme = Theme(
         emojis: ["🍄","🌷","🌺","🌸","🌼","🌻","🥀","💐","🌾", "🍀", "☘️", "🌵"],
         backgroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1),
         cardColor: #colorLiteral(red: 0.8446564078, green: 0.5145705342, blue: 1, alpha: 1)
     )
     
-    static let themes = [halloweenTheme, facesTheme, handsTheme, sportsTheme, animalsTheme, natureTheme]
+    private static let themes = [halloweenTheme, facesTheme, handsTheme, sportsTheme, animalsTheme, natureTheme]
     
-    var theme = randomTheme()
+    private var theme = randomTheme()
         
-    var emoji = [Int:String]()
+    private var emoji = [Card:String]()
     
     var score = 0
     
-    var scoreEmojis = [ClickedEmoji]()
+    private var scoreEmojis = [ClickedEmoji]()
     
-    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet private weak var flipCountLabel: UILabel!
     
-    @IBOutlet weak var scoreCountLabel: UILabel!
+    @IBOutlet private weak var scoreCountLabel: UILabel!
     
-    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet private weak var newGameButton: UIButton!
     
-    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet private var cardButtons: [UIButton]!
     
-    @IBAction func newGame(_ sender: UIButton) {
+    @IBAction private func newGame(_ sender: UIButton) {
         game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
         flipCountLabel.text = "Flips: \(game.getFlipCount())"
         theme = ViewController.randomTheme()
@@ -75,7 +79,7 @@ class ViewController: UIViewController {
         scoreEmojis = [ClickedEmoji]()
     }
     
-    @IBAction func touchCard(_ sender: UIButton) {
+    @IBAction private func touchCard(_ sender: UIButton) {
         let flipCount = game.incrementFlipCount()
         flipCountLabel.text = "Flips: \(flipCount)"
         if let cardNumber = cardButtons.firstIndex(of: sender) {
@@ -87,7 +91,7 @@ class ViewController: UIViewController {
         }
     }
     
-    func updateViewFromModel() {
+    private func updateViewFromModel() {
         view.backgroundColor = theme.backgroundColor
         flipCountLabel.textColor = theme.cardColor
         scoreCountLabel.textColor = theme.cardColor
@@ -106,7 +110,7 @@ class ViewController: UIViewController {
         }
     }
     
-    static func randomTheme() -> Theme {
+    private static func randomTheme() -> Theme {
         if let randomTheme = themes.randomElement() {
             return randomTheme
         } else {
@@ -114,15 +118,14 @@ class ViewController: UIViewController {
         }
     }
     
-    func emoji(for card: Card) -> String {
-        if emoji[card.identifier] == nil, theme.emojis.count > 0 {
-            let randomIndex = Int(arc4random_uniform(UInt32(theme.emojis.count)))
-            emoji[card.identifier] = theme.emojis.remove(at: randomIndex)
+    private func emoji(for card: Card) -> String {
+        if emoji[card] == nil, theme.emojis.count > 0 {
+            emoji[card] = theme.emojis.remove(at: theme.emojis.count.arc4random)
         }
-        return emoji[card.identifier] ?? "?"
+        return emoji[card] ?? "?"
     }
         
-    func calculateGameScore(for sender: UIButton) -> Int {
+    private func calculateGameScore(for sender: UIButton) -> Int {
         let card = game.cards
         let button = sender
         for index in cardButtons.indices {
@@ -156,5 +159,17 @@ class ViewController: UIViewController {
             }
         }
         return score
+    }
+}
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
     }
 }
